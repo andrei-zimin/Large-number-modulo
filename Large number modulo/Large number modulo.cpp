1a1227ff29fefa5e0e7e8a0e858b761f9cb25379
cherpontov <62599638+cherpontov@users.noreply.github.com>
2023-07-07 03:06:18 +0300

    First Commit
    First Commit

---
+++ Large number modulo/Large number modulo.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int modebasep(int base, int degree, int mod)
+{
+	if (degree == 0) return 1;
+	int z = modebasep(base, degree / 2, mod);
+
+	if (degree % 2 == 0)
+		return (z * z) % mod;
+	else
+		return (base * z * z) % mod;
+}
+
+int main()
+{
+	while (1)
+	{
+		int base, degree, mod;
+
+		cout << "base= "; cin >> base;
+		cout << "degree= "; cin >> degree;
+		cout << "mod= "; cin >> mod;
+
+		cout << modebasep(base, degree, mod);
+
+		cout << "\n\n\n";
+
+	}
+	return 0;
+}
